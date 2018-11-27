74512764c324c1caadc37560a7a4b7d61c1c64c0
youssefrif14 <45339437+youssefrif14@users.noreply.github.com>
2018-11-27 22:09:17 +0300

    Add files via upload

--- wes.js
+++ wes.js
@@ -7,19 +7,19 @@ console.log(`Logged in as ${client.user.tag}!`);
 
 
 client.on('ready', async() => {
-var server = "516318670016741397";//ايدي السيرفر
-var channel = "516318840666324992";//ايدي الروم
+var server = "512556758833823764";//ايدي السيرفر
+var channel = "512556850558795777";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا ')
 	},100);
 })
 
 client.on('message', message => {
-    let prefix = 'say';
+    let prefix = 'البريفكس';
    if(message.content.startsWith('say')) {
        let args = message.content.split(' ').slice(1).join(' ');
        message.channel.send(args)
    } 
 });
 
-client.login("NTE3MDA5NzQ4MTM5MzExMTA3.Dt8GKw.PgUcTtPLxr6AEbA0B66juKXAYj0");
+client.login("توكن");
