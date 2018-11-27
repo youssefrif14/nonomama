b4c5be22712139e37769dac8a39a0b9e9d8d9290
youssefrif14 <45339437+youssefrif14@users.noreply.github.com>
2018-11-27 21:14:52 +0300

    Update wes.js

--- wes.js
+++ wes.js
@@ -23,28 +23,3 @@ client.on('message', message => {
 });
 
 client.login("NTE3MDA5NzQ4MTM5MzExMTA3.Dt8GKw.PgUcTtPLxr6AEbA0B66juKXAYj0");
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-console.log(`Logged in as ${client.user.tag}!`);
-});
-
-
-client.on('ready', async() => {
-var server = "516318670016741397";//ايدي السيرفر
-var channel = "516318840666324992";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا ')
-	},100);
-})
-
-client.on('message', message => {
-    let prefix = 'say';
-   if(message.content.startsWith('say')) {
-       let args = message.content.split(' ').slice(1).join(' ');
-       message.channel.send(args)
-   } 
-});
-
-client.login("NTE3MDM3MTAyOTQ1MjcxODA4.Dt8X0w.C0ueC0cLdQGXHANySgEnbNUa6vg");
